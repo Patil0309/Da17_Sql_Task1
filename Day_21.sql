d661cf57bda83ba2b5fa20022a942da4eccbb088
patil0512 <162035779+patil0512@users.noreply.github.com>
2025-03-30 23:41:11 +0530

    create and insert sql basic example1

---
+++ Day_21.sql
@@ -0,0 +1,9 @@
+select*from student
+insert into student (id,name,isactive,mark,subject) values(1,'user1',true,35,'sub1')
+
+insert into student (id,name,isactive,mark,subject) values('2','user2',true,44,'sub2')
+insert into student (id,isactive,mark,subject) values(4,true,50,'sub4')
+
+insert into student (id,name,isactive,mark,subject) values
+(7,'user7',false,77,'sub2'),
+(8,'user8',false,90,'sub3')
